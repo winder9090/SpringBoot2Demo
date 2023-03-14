b3d112f021b6dfb7db5ba499d506acc7847748f6
winder9090 <winder9090@gmail.com>
2023-03-14 15:04:15 +0800

    创建初始化项目

---
+++ src/test/java/jingweng/demo/springboot2/SpringBoot2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jingweng.demo.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
