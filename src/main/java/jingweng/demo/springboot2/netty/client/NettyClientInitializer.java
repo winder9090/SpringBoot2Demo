47a43ebb38782481b8d44e061a525feb73b02b96
winder9090 <winder9090@gmail.com>
2023-03-26 09:11:29 +0800

    1.添加Netty Client连接 2.添加Netty Server连接

---
+++ src/main/java/jingweng/demo/springboot2/netty/client/NettyClientInitializer.java
@@ -0,0 +1,15 @@
+package jingweng.demo.springboot2.netty.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class NettyClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast("decoder", new StringDecoder());
+        socketChannel.pipeline().addLast("encoder", new StringEncoder());
+        socketChannel.pipeline().addLast(new NettyClientHandler());
+    }
+}
---
+++ src/main/java/jingweng/demo/springboot2/netty/server/ServerChannelInitializer.java
@@ -0,0 +1,20 @@
+package jingweng.demo.springboot2.netty.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 通道初始化
+ */
+public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        //添加编解码
+        socketChannel.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+        socketChannel.pipeline().addLast(new NettyServerHandler());
+    }
+}
